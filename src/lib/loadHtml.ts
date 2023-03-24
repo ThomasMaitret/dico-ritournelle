29c38f3a811a4d1ba3da612c3df57e283044b8e4
t.maitret-richard-extern@renault.com <t.maitret-richard-extern@renault.com>
2023-03-25 00:13:15 +0100

    fix import

--- src/lib/loadHtml.ts
+++ src/lib/loadHtml.ts
@@ -1,9 +1,9 @@
+import { load } from "cheerio";
 import type { CheerioAPI } from "cheerio";
 
 export async function loadHtml(url: string): Promise<CheerioAPI | null> {
   const response = await fetch(url, { mode: "no-cors" });
   if (!response.ok) return null;
   const data = await response.text();
-  const { load } = await import("./cheerio-facade");
   return load(data);
 }
