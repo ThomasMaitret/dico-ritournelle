252c7a7bdd111ca6ae66b3a35ce143ea0dd64bdf
Thomas Maitret-Richard <thomas.maitret-richard-extern@renault.com>
2025-08-30 22:35:29 +0200

    fix proxy

--- vite.config.js
+++ vite.config.js
@@ -3,30 +3,35 @@ import { defineConfig } from "vite";
 export default defineConfig({
   server: {
     proxy: {
-      "^/academie/.*": {
+      "/academie": {
         target: "https://academie.atilf.fr/9/consulter",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/academie/, ""),
+        secure: false,
       },
-      "^/cnrtl/.*": {
+      "/cnrtl": {
         target: "https://www.cnrtl.fr/definition",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/cnrtl/, ""),
+        secure: false,
       },
-      "^/larousse/.*": {
+      "/larousse": {
         target: "https://www.larousse.fr/dictionnaires/francais",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/larousse/, ""),
+        secure: false,
       },
-      "^/littre/.*": {
+      "/littre": {
         target: "https://www.littre.org/definition",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/littre/, ""),
+        secure: false,
       },
-      "^/robert/.*": {
+      "/robert": {
         target: "https://dictionnaire.lerobert.com/definition",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/robert/, ""),
+        secure: false,
       },
     },
   },
